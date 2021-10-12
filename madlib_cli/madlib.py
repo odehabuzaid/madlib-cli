0ed13ff836ea834d2214fe4d672173e231bed202
Odeh <abuzaid163@hotmail.com>
2021-10-12 20:11:06 +0300

    get user  input and merge

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -11,58 +11,49 @@ def welcome():
         - explaining the  process
         - command line interactions
     """
-    logo = Figlet(font='standard')
-    print(colored(logo.renderText('madLib'), "red"))
-    print(colored(logo.renderText("Hello ,{}\n".format(getpass.getuser())), "cyan"))    
-    print(colored(('='*80), "red"))
+    logo = Figlet(font="standard")
+    print(colored(logo.renderText("madLib"), "red"))
+    print(colored(logo.renderText("Hello ,{}\n".format(getpass.getuser())), "cyan"))
+    print(colored(("=" * 80), "red"))
     print(colored(("Welcome to madLib!"), "white"))
-    print(colored(("This program will help you create a madlib from a template."), "white"))
+    print(
+        colored(
+            ("This program will help you create a madlib from a template."), "white"
+        )
+    )
     print(colored(("You will be prompted for words to fill in the blanks."), "white"))
     input(colored(("Press enter to continue..."), "cyan"))
-    print(colored(('='*80), "red"))
+    print(colored(("=" * 80), "red"))
 
-welcome()
+
+def main():
+    """
+    - [x] Read a template Madlib file. and parse that file into usable parts.
+    - [x] Prompt the user to submit a series of words to fit each of the required components of the Madlib template.
+    - [x] With the collected user inputs, populate the template such that each provided input is placed into the    correct position within the template.
+    """
+    path = "./Madlib .txt"
+    file_content = read_template(path)
+    language_parts, content = parse_template(file_content)
+    print(type(language_parts))
+    print(type(content))
+    print(colored(content, "green"))
+    print(colored(("\n Enter the words to fill in the blanks :\n"), "cyan"))
+    user_guesses = []
+    for part in language_parts:
+        guess = input(colored(("{} : ".format(part)), "cyan"))
+        user_guesses.append(guess)
+
+    merge(content, user_guesses)
+
+
 """
-Read a template Madlib file. and parse that file into usable parts.
-
-Prompt the user to submit a series of words to fit each of the required components of the Madlib template.
-
-With the collected user inputs, populate the template such that each provided input is placed into the correct position within the template.
-
-Make Me A Video Game!
-
-I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb}{A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!
-! I the majestic and purple Scott have colored JB's laughing sister and plan to steal her tickled arrows!
-
-What are a {Large Animal} and backpacking {Small Animal} to do? 
-! What are a gorilla and backpacking butterfly to do?
-Before you can help {A Girl's Name}, 
-! Before you can help Betty,
-you'll have to collect the {Adjective} {Plural Noun} 
-! you'll have to collect the silly tests
-and {Adjective} {Plural Noun} that open up the {Number 1-50} 
-! and striped jackets that open up the 44 
-worlds connected to A {First Name's} Lair. 
-! worlds connected to Wilson's' Lair.
-There are {Number} {Plural Noun} and {Number} {Plural Noun} in the game, 
-! There are 3 leaves and 4 swords in the game,
-along with hundreds of other goodies for you to find.
-
 After the resulting Madlib has been completed, provide the completed response back to the user in the command line.
 
 Write the completed text (!in red above)to a new file on your file system (in the repo).
 
 Note: A smaller example file is included as well which can be handy when developing/testing.
 ! It was a {Adjective} and {Adjective} {Noun}.
-
-*. Create and test a read_template function that takes in 
-*. a path to text file 
-*. returns a stripped string of the file’s contents.
-!. read_template should raise a FileNotFoundError if path is invalid.
-
-*. Create and test a parse_template function that takes in
-*. template string 
-*. returns a string with language parts removed and a separate list of those language parts.
 
 *. Create and test a merge function that takes in 
 *. a “bare” template and a list of user entered language parts,
@@ -78,7 +69,6 @@ Note: A smaller example file is included as well which can be handy when develop
     TODO [x] : create a function that takes path and returns a stripped string of the file’s contents.
     TODO [x] : create a function that takes template string and returns a string with language parts removed and a separate list of those language parts.    
     TODO [x] : create a function that takes a “bare” template and a list of user entered language parts, and returns a string with the language parts inserted into the template.
-
 """
 
 
@@ -97,17 +87,23 @@ def parse_template(content: str):
     for part in language_parts:
         content = re.sub("{}".format(part), "", content)
 
-    return tuple(language_parts),content
+    return tuple(language_parts), content
 
 
-def merge():
-    print()
+def merge(*args):
+    content, user_guesses = args
+    for part in user_guesses:
+        content = content.replace("{}".format("{}"), "{} ".format(part), 1)
+    
+    content = content.strip()
+    return content
 
 
-path = "./Madlib .txt"
-file_content = read_template(path)
+if __name__ == "__main__":
+    welcome()
+    main()
 
 
-content, language_parts = parse_template(file_content)
+# content, language_parts = parse_template(file_content)
 
 # print(parse_template("It was a {Adjective} and {Adjective} {Noun}."))
